263b8540304ae5484893e6713f0068f5e8a13190
Reza <99068159@mydavinci.nl>
2023-04-14 10:10:02 +0200

    Heb aangepast dat de code nu ook werkt vanaf het begin zonder dat je op een link hoeft te klikken

--- index.php
+++ index.php
@@ -16,7 +16,8 @@
   
 	<!-- laad hier via php de juiste contentpagina in (vanuit de pages map) in. Welke geselecteerd moet worden kun je uit de URL halen (URL_Params).-->
   <?php 
-  
+  if (array_key_exists('page', $_GET) && $_GET['page'] == 'osu') {
+
     if($_GET['page'] == 'eldenring') {
         include('pages/onderwerp1.php');
     } else if($_GET['page'] == 'hollow-knight') {
@@ -24,6 +25,10 @@
     } else if($_GET['page'] == 'osu') {
       include('pages/onderwerp3.php');
     }
+
+  } else {
+    include('pages/onderwerp1.php');
+  }
 
     ?>
 	
