d8bbc1d0ac1831c11cb586bd3ff987efa0e92176
Reza <99068159@mydavinci.nl>
2023-04-14 09:46:37 +0200

    Heb de derde pagina toegevoegd en de variabele ge defined voor de error code in het begin

--- index.php
+++ index.php
@@ -15,11 +15,16 @@
 ?>
   
 	<!-- laad hier via php de juiste contentpagina in (vanuit de pages map) in. Welke geselecteerd moet worden kun je uit de URL halen (URL_Params).-->
-  <?php if($_GET['name'] == 'eldenring') {
+  <?php 
+    $_GET['name'] = '';
+
+    if($_GET['name'] == 'eldenring') {
         include('pages/onderwerp1.php');
     } else if($_GET['name'] == 'hollow-knight') {
         include('pages/onderwerp2.php');
-    } 
+    } else {
+      include('pages/onderwerp3.php');
+    }
     ?>
 	
 	<!-- laad hier via php je footer in (vanuit je includes map)-->
